379b434cdeadb67dc1abc69626a09635520533f5
Franco Silvani <fsilvani@Francos-MacBook-Air.local>
2017-01-24 15:34:52 -0300

    actualiza archivos segun WIP de home office

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 'use strict';
 const querystring = require('querystring');
 const http = require('http');
+const request = require('request');
 
 exports.iniciar = (req, res) => {
   console.log('Peticionó iniciar: ', req.path);
@@ -15,43 +16,65 @@ exports.views = (req, res) => {
 exports.addCampaign = (req, res) => {
   console.log('Peticionó: ', req.path);
 
-  let form_data = req.body;
+  let formData = req.body;
 
-  let post_data = Object.assign({
+  let postData = Object.assign({
     "campaign_type":"SERVER_BEHAVIOR",
-  }, form_data);
+  }, formData);
+
+  postData.priority = Number(postData.priority);
+  postData.segment_size = Number(postData.segment_size);
+  postData.start_date = '2017-01-17T00:00:00.000-03:00';
+  postData.end_date = '2017-01-20T00:00:00.000-03:00';
+  postData.segments = [];
+
+  // hardcodeo 4 para luego analizar refactor para volver dinámico el agregado de segmentos
+  for (var i = 1; i <= 4; i++) {
+    postData.segments.push({
+      type: 'QUESTION',
+      tag: postData[`segment${i}_tag`],
+      questions_id: postData[`segment${i}_questions_id`]
+    });
+    delete postData[`segment${i}_tag`];
+    delete postData[`segment${i}_questions_id`];
+  }
+
+  console.log(postData);
 
   // Build the post string from an object
-  // post_data = querystring.stringify(post_data);
+  postData = querystring.stringify(postData);
 
-  console.log(post_data);
-
-  // An object of options to indicate where to post to
-  const post_options = {
+  const options = {
     host: 'host',
-    port: '8087',
-    path: '/api/path',
+    port: 8087,
+    path: '/api/...',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(post_data)
+      'Content-Length': Buffer.byteLength(postData)
     }
   };
-  // Set up the request
-  const post_req = http.request(post_options, res => {
+
+  const request = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
     res.setEncoding('utf8');
-    res.on('data', chunk => {
-        console.log('Response: ' + chunk);
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
     });
   });
 
-  post_req.on('error', e => {
-    console.log('problem with request: ' + e.message);
+  request.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
   });
 
-  // post the data
-  post_req.write(post_data);
-  post_req.end();
+  // write data to request body
+  // request.write(postData);
+  request.write(postData);
+  request.end();
 
   // res.sendFile('./views/createForm.html', { root: __dirname });
 };
